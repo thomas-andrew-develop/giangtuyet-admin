aa5949b767e9f9d7228d928a0dd4f7e75ea58054
phgiang1987 <phgiang1987@gmail.com>
2023-08-25 09:30:35 +0700

    update

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
             publicRoutes.map((route, index) => {
               const Layout = route.layout === null ? Fragment : DefaultLayout; 
               const Page = route.component;
-              return <Route key={index} path={route.path} element={<Layout><Page /></Layout>} />
+              return <Route key={index} path={route.path} exact element={<Layout><Page /></Layout>} />
             })
           }
         </Routes>
